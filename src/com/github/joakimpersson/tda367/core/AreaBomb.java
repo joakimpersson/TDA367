348d88142b2b536ddd7e1f87e3a97697b055d8fb
Joakim Persson <joakimperssson@gmail.com>
2012-03-25 23:03:33 +0200

    Added skeleton

---
+++ src/com/github/joakimpersson/tda367/core/AreaBomb.java
@@ -0,0 +1,14 @@
+package com.github.joakimpersson.tda367.core;
+
+public class AreaBomb extends Bomb {
+
+	public AreaBomb(Player player) {
+		super(player);
+	}
+
+	@Override
+	public void explode() {
+		// TODO Add implentation
+	}
+
+}
---
+++ src/com/github/joakimpersson/tda367/core/NormalBomb.java
@@ -0,0 +1,15 @@
+package com.github.joakimpersson.tda367.core;
+
+public class NormalBomb extends Bomb{
+
+	public NormalBomb(Player player) {
+		super(player);
+	}
+	
+	@Override
+	public void explode() {
+		// TODO Not implemented
+		
+	}
+
+}
