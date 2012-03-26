977a8faea38844be0c3440f91df5d1b28827993a
Viktor Anderling <natanande@gmail.com>
2012-03-26 20:58:07 +0200

    fix

--- src/com/github/joakimpersson/tda367/core/GameField.java
+++ src/com/github/joakimpersson/tda367/core/GameField.java
@@ -1,18 +1,29 @@
 package com.github.joakimpersson.tda367.core;
 
 /**
- * 
- * @author Viktor
+ * @date 2012-03-26
+ * @author Viktor Anderling
  * 
  * An abstract class representing the gamefield with a matrix of tiles.
+ * 
  */
 public abstract class GameField {
 	private Tile[][] map;
-
-	public abstract void createWorld();
-
+	
+	
+	/**
+	 * Resets the field to it's initial state.
+	 */
 	public abstract void resetField();
 
+	/**
+	 * A method for setting a specific position of the gamefield to a specific tile.
+	 * 
+	 * @param tile	The tile that will be set.
+	 * @param pos	The position where the tile will be set.
+	 * @throws IndexOutBoundsException if the position has negative parameters or is
+	 * bigger than the size of the matrix.
+	 */
 	public void setTile(Tile tile, Position pos) {
 		int x = pos.getX();
 		int y = pos.getY();
