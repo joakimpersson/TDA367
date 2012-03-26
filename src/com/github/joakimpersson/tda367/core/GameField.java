a7f49dadfa53d6646c24dae979de0d06bf35ecbe
Viktor Anderling <natanande@gmail.com>
2012-03-26 20:50:27 +0200

    finished gamefield

--- src/com/github/joakimpersson/tda367/core/GameField.java
+++ src/com/github/joakimpersson/tda367/core/GameField.java
@@ -1,5 +1,11 @@
 package com.github.joakimpersson.tda367.core;
 
+/**
+ * 
+ * @author Viktor
+ * 
+ * An abstract class representing the gamefield with a matrix of tiles.
+ */
 public abstract class GameField {
 	private Tile[][] map;
 
@@ -8,7 +14,12 @@ public abstract class GameField {
 	public abstract void resetField();
 
 	public void setTile(Tile tile, Position pos) {
-
+		int x = pos.getX();
+		int y = pos.getY();
+		if(x < 0 || y < 0 || x > map.length || y < map[0].length) {
+			throw new IndexOutOfBoundsException();
+		}
+		map[x][y] = tile;
 	}
 
 }
