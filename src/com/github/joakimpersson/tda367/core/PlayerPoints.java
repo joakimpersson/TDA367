a04d6ed53ef9282b22ebc2c03658e9fb20234875
Andreas Rolen <adderollen@hotmail.com>
2012-03-27 14:36:54 +0200

    Implemented PlayerPoints and created Highscore after the design model

--- src/com/github/joakimpersson/tda367/core/PlayerPoints.java
+++ src/com/github/joakimpersson/tda367/core/PlayerPoints.java
@@ -1,6 +1,7 @@
 package com.github.joakimpersson.tda367.core;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.github.joakimpersson.tda367.core.tiles.Tile;
@@ -28,22 +29,36 @@ public class PlayerPoints {
 	
 	private int totalScore;
 	private int credits;
-	private List<Tile>destroyedTiles;
+	private HashMap<String, Integer> destroyedTiles;
 	private int killedPlayers;
 	
 	public PlayerPoints() {
 		this.totalScore = 0;
 		this.credits = 0;
 		this.killedPlayers = 0;
-		this.destroyedTiles = new ArrayList<Tile>();
+		this.destroyedTiles = new HashMap<String, Integer>();
+		this.destroyedTiles.put("Pillar", 0);
+		this.destroyedTiles.put("Box", 0);
 	}
 	
 	public void update(List<PointGiver>list) {
 		
+		for(int i = 0; i < list.size(); i++) {
+			this.totalScore += list.get(i).getScore();
+			this.credits += list.get(i).getScore();
+			
+			if(list.get(i).getScore() == 30) {
+				this.killedPlayers = this.killedPlayers + 1;
+			} else if(list.get(i).getScore() == 20)	{
+				this.destroyedTiles.put("Pillar", this.destroyedTiles.get("Pillar") + 1);
+			} else if(list.get(i).getScore() == 5) {
+				this.destroyedTiles.put("Box", this.destroyedTiles.get("Box") + 1);
+			}	
+		}
 	}
 	
 	public void reduceCredits(int cost) {
-		
+		this.credits = this.credits - cost;
 	}
 	
 	public int getScore() {
@@ -52,9 +67,5 @@ public class PlayerPoints {
 	
 	public int getCredits() {
 		return this.credits;
-	}
-	
-	public void reset() {
-		
 	}
 }
