308ef72ce62decf4a409fe9a986f6d1240f34a7c
Andreas Rolen <adderollen@hotmail.com>
2012-03-27 16:29:48 +0200

    Highscore done, minor changes in PlayerPoints

--- src/com/github/joakimpersson/tda367/core/Highscore.java
+++ src/com/github/joakimpersson/tda367/core/Highscore.java
@@ -1,5 +1,43 @@
 package com.github.joakimpersson.tda367.core;
 
+import java.util.ArrayList;
+
 public class Highscore {
+
+	private ArrayList<Player> players;
+
+	public void update(ArrayList<Player> otherPlayers) {
+
+		if (this.players.isEmpty()) {
+			this.players.add(otherPlayers.get(0));
+		}
+
+		for (int i = 0; i < otherPlayers.size(); i++) {
+			for (int j = 0; j < this.players.size(); j++) {
+				if (otherPlayers.get(i).getScore() > this.players.get(j)
+						.getScore()) {
+					this.players.add(j, otherPlayers.get(i));
+				} else if (j == this.players.size() - 1) {
+					this.players.add(j, otherPlayers.get(i));
+				}
+			}
+		}
+	}
+	
+	public ArrayList<Player> getList() {
+		return (ArrayList<Player>) this.players.clone();
+	}
+	
+	public Player getPlayer(int position) {
+		return this.players.get(position);
+	}
+	
+	public int getSize() {
+		return this.players.size();
+	}
+	
+	public void clear() {
+		this.players.clear();
+	}
 
 }
