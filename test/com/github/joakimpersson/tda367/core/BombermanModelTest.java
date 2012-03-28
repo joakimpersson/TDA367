f38017c6b5133b1e9a8437043da9c7fd636a6f0f
Viktor Anderling <natanande@gmail.com>
2012-03-28 22:41:14 +0200

    Added test case for bombermanModel

---
+++ test/com/github/joakimpersson/tda367/core/BombermanModelTest.java
@@ -0,0 +1,54 @@
+package com.github.joakimpersson.tda367.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BombermanModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpgradePlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHandleFire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTileToughnessPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTileToughnessIntInt() {
+		fail("Not yet implemented");
+	}
+
+}
