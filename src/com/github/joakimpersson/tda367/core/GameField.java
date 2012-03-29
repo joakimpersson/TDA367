033499d67c02c27b45014396d4cf43955f921e24
Joakim Persson <joakimperssson@gmail.com>
2012-03-29 18:28:15 +0200

    Removed unnecessary methods and fixed nullpointerexception in StandradMap

--- src/com/github/joakimpersson/tda367/core/GameField.java
+++ src/com/github/joakimpersson/tda367/core/GameField.java
@@ -4,49 +4,48 @@ import java.awt.Dimension;
 
 import com.github.joakimpersson.tda367.core.tiles.Tile;
 
-
-
 /**
  * @date 2012-03-26
  * @author Viktor Anderling
+ * @modified Joakim Persson
  * 
- * An abstract class representing the game-field with a matrix of tiles.
+ *           An abstract class representing the game-field with a matrix of
+ *           tiles.
  * 
  */
 public abstract class GameField {
 	private Tile[][] map;
-	
+
 	public GameField(int xSize, int ySize) {
 		this.map = new Tile[ySize][xSize];
 	}
-	
-	/**
-	 * A method that is used to create the game-field, but also functions
-	 * as way to reset it to it initial state.
-	 */
-	public abstract void resetField();
-	
 
 	/**
-	 * A method for setting a specific position of the game-field to a specific tile.
+	 * A method that is used to create the game-field, but also functions as way
+	 * to reset it to it initial state.
+	 */
+	public abstract void resetField();
+
+	/**
+	 * A method for setting a specific position of the game-field to a specific
+	 * tile.
 	 * 
-	 * @param tile	The tile that will be set.
-	 * @param pos	The position where the tile will be set.
+	 * @param tile
+	 *            The tile that will be set.
+	 * @param pos
+	 *            The position where the tile will be set.
 	 */
 	public void setTile(Tile tile, Position pos) {
 		int x = pos.getX();
 		int y = pos.getY();
-		this.setTile(tile, x, y);
-	}
-	
-	public void setTile(Tile tile, int x, int y) {
 		checkPos(x, y);
 		map[y][x] = tile;
 	}
-	
+
 	/**
 	 * 
-	 * @param pos The position of the sought tile.
+	 * @param pos
+	 *            The position of the sought tile.
 	 * @return The tile of that position.
 	 */
 	public Tile getTile(Position pos) {
@@ -55,33 +54,30 @@ public abstract class GameField {
 		checkPos(x, y);
 		return map[y][x];
 	}
-	
-	public Tile getTile(int x, int y) {
-		return this.getTile(new Position(x, y));
-	}
-	
+
 	public Tile[][] getMap() {
 		return this.map;
 	}
-	
+
 	/**
 	 * @return The dimension of the game-field matrix.
 	 */
 	public Dimension getSize() {
 		return new Dimension(map.length, map[0].length);
 	}
-	
+
 	/**
 	 * A method for checking if coordinates are valid.
 	 * 
-	 * @param x 
+	 * @param x
 	 * @param y
-	 * @throws IndexOutBoundsException if the position has negative parameters or is
-	 * bigger than the size of the matrix.
+	 * @throws IllegalArgumentException
+	 *             if the position has negative parameters or is bigger than the
+	 *             size of the matrix.
 	 */
-	private void checkPos(int x, int y) {
-		if(x < 0 || y < 0 || y > map.length || x < map[0].length) {
-			throw new IndexOutOfBoundsException();
+	private void checkPos(int x, int y) throws IllegalArgumentException {
+		if (x < 0 || y < 0 || y > map.length || x > map[0].length) {
+			throw new IllegalArgumentException();
 		}
 	}
 }
