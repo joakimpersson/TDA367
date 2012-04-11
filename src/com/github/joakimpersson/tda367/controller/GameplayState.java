4ea95ec53e539a77eefbd23754737196c45bbd7b
Joakim Persson <joakimperssson@gmail.com>
2012-04-11 15:44:39 +0200

    Continued working with the gameplay view

--- src/com/github/joakimpersson/tda367/controller/GameplayState.java
+++ src/com/github/joakimpersson/tda367/controller/GameplayState.java
@@ -2,18 +2,26 @@ package com.github.joakimpersson.tda367.controller;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import com.github.joakimpersson.tda367.gui.GameplayView;
+import com.github.joakimpersson.tda367.gui.IView;
+import com.github.joakimpersson.tda367.model.BombermanModel;
+import com.github.joakimpersson.tda367.model.IBombermanModel;
+
 /**
  * 
  * @author joakimpersson
- *
+ * 
  */
 public class GameplayState extends BasicGameState {
 
 	private int stateID = -1;
+	private IBombermanModel model = null;
+	private IView view = null;
 
 	public GameplayState(int stateID) {
 		this.stateID = stateID;
@@ -22,21 +30,26 @@ public class GameplayState extends BasicGameState {
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		model = BombermanModel.getInstance();
+		view = new GameplayView();
 
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-		// TODO Auto-generated method stub
-
+		view.render(container, game, g);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		// TODO jocke only used during development
+		Input input = container.getInput();
+
+		if (input.isKeyDown(Input.KEY_ESCAPE)) {
+			container.exit();
+		}
 
 	}
 
--- src/com/github/joakimpersson/tda367/gui/GameplayView.java
+++ src/com/github/joakimpersson/tda367/gui/GameplayView.java
@@ -1,5 +1,6 @@
 package com.github.joakimpersson.tda367.gui;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -8,10 +9,14 @@ import org.newdawn.slick.state.StateBasedGame;
 import com.github.joakimpersson.tda367.model.BombermanModel;
 import com.github.joakimpersson.tda367.model.IBombermanModel;
 import com.github.joakimpersson.tda367.model.tiles.Tile;
+import com.github.joakimpersson.tda367.model.tiles.nonwalkable.Box;
+import com.github.joakimpersson.tda367.model.tiles.nonwalkable.Pillar;
+import com.github.joakimpersson.tda367.model.tiles.nonwalkable.Wall;
+import com.github.joakimpersson.tda367.model.tiles.walkable.Floor;
 
 public class GameplayView implements IView {
 
-	// TODO jocke ask wheater this is ok!
+	// TODO jocke ask if this is okay!
 	private IBombermanModel model = null;
 
 	public GameplayView() {
@@ -20,7 +25,7 @@ public class GameplayView implements IView {
 	}
 
 	private void init() {
-		
+
 	}
 
 	@Override
@@ -32,6 +37,38 @@ public class GameplayView implements IView {
 
 	private void drawMap(GameContainer container, Graphics g) {
 		Tile[][] map = model.getMap();
-		
+		int mapHeight = map.length;
+		int mapWidth = map[0].length;
+
+		int blockWidth = container.getWidth() / mapWidth;
+		int blockHeight = container.getHeight() / mapHeight;
+
+		for (int i = 0; i < mapHeight; i++) {
+			for (int j = 0; j < mapWidth; j++) {
+				Tile tile = map[i][j];
+				if (tile instanceof Wall) {
+					drawRect(blockWidth * j, blockHeight * i, blockWidth,
+							blockHeight, Color.pink, g);
+				} else if (tile instanceof Floor) {
+					drawRect(blockWidth * j, blockHeight * i, blockWidth,
+							blockHeight, Color.cyan, g);
+				} else if (tile instanceof Pillar) {
+					drawRect(blockWidth * j, blockHeight * i, blockWidth,
+							blockHeight, Color.orange, g);
+				} else if (tile instanceof Box) {
+					drawRect(blockWidth * j, blockHeight * i, blockWidth,
+							blockHeight, Color.yellow, g);
+				}
+			}
+		}
+
+	}
+
+	private void drawRect(final int x, final int y, final int width,
+			final int height, final Color color, final Graphics g) {
+		g.setColor(color);
+		g.fillRect(x, y, width, height);
+		g.setColor(Color.black);
+		g.drawRect(x, y, width, height);
 	}
 }
