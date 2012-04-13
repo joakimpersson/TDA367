e1a636eb73c3ab2282822e5e4ae3e8507294b4d8
Joakim Persson <joakimperssson@gmail.com>
2012-04-13 03:05:30 +0200

    Added a view and controller for the highscoreview

---
+++ src/com/github/joakimpersson/tda367/controller/HighscoreState.java
@@ -0,0 +1,53 @@
+package com.github.joakimpersson.tda367.controller;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import com.github.joakimpersson.tda367.gui.HighscoreView;
+import com.github.joakimpersson.tda367.gui.IView;
+
+public class HighscoreState extends BasicGameState {
+
+	private int stateID = -1;
+	private IView view = null;
+
+	public HighscoreState(int stateID) {
+		this.stateID = stateID;
+	}
+
+	@Override
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException {
+		this.view = new HighscoreView();
+
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g)
+			throws SlickException {
+		view.render(container, game, g);
+
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta)
+			throws SlickException {
+		Input input = container.getInput();
+
+		// TODO jocke only used during development
+		if (input.isKeyDown(Input.KEY_ESCAPE)) {
+			container.exit();
+		}
+
+	}
+
+	@Override
+	public int getID() {
+		return stateID;
+	}
+
+}
