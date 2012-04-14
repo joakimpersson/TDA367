e146639ad5e194b33dcc9caa8cbb41805efeae7d
Adrian Bjugård <adrian@bjugard.se>
2012-04-14 17:49:03 +0200

    added toString() :P

--- src/com/github/joakimpersson/tda367/model/constants/Direction.java
+++ src/com/github/joakimpersson/tda367/model/constants/Direction.java
@@ -1,17 +1,19 @@
 package com.github.joakimpersson.tda367.model.constants;
 
 public enum Direction {
-	Up (0, -1), 
-	Down (0, 1), 
-	Left (-1, 0), 
-	Right (1, 0), 
-	None (0, 0);
+	Up (0, -1, "Up"), 
+	Down (0, 1, "Down"), 
+	Left (-1, 0, "Left"), 
+	Right (1, 0, "Right"), 
+	None (0, 0, "None");
 	
 	private final int x, y;
+	private final String s;
 	
-	Direction(int x, int y) {
+	Direction(int x, int y, String s) {
 		this.x = x;
 		this.y = y;
+		this.s = s;
 	}
 	
 	public int getX() {
@@ -20,5 +22,9 @@ public enum Direction {
 	
 	public int getY() {
 		return y;
+	}
+	
+	public String toString() {
+		return s;
 	}
 }
