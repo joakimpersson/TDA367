2928d2b5ebacd76092e91719a30917ff9313c322
Joakim Persson <joakimperssson@gmail.com>
2012-04-16 22:35:10 +0200

    Refactor and added an extra view responsible for showing a players attributes

---
+++ src/com/github/joakimpersson/tda367/gui/UpgradePlayerPanelView.java
@@ -0,0 +1,87 @@
+package com.github.joakimpersson.tda367.gui;
+
+import java.util.Map;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+
+import com.github.joakimpersson.tda367.model.constants.Attribute;
+import com.github.joakimpersson.tda367.model.player.Player;
+
+/**
+ * 
+ * @author joakimpersson
+ *
+ */
+public class UpgradePlayerPanelView implements IUpgradePlayerView {
+	//TODO jocke rethink design
+	private Player player;
+	private final int X;
+	private final int Y;
+	private int yDelta = -1;
+
+	public UpgradePlayerPanelView(Player p, int x, int y) {
+		this.player = p;
+		this.X = x;
+		this.Y = y;
+		System.out.println(x + " " + y);
+		init();
+	}
+
+	private void init() {
+		yDelta = 50;
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g,
+			Map<Player, Integer> playerAttrIndex) {
+
+		int posX = X;
+		int posY = Y;
+
+		// TODO jocke yeah i know that it is a mess
+		g.setColor(Color.white);
+
+		String str = formatHeaderString();
+		g.drawString(str, posX, posY);
+
+		posY += 25;
+
+		g.drawString("name(lvl), cost", posX, posY);
+
+		int index = 0;
+		for (Attribute a : player.getPermanentAttributes()) {
+			posY += yDelta;
+
+			if (index == playerAttrIndex.get(player)) {
+				g.setColor(Color.cyan);
+			}
+			str = formatAttrString(a);
+			g.drawString(str, posX, posY);
+			// make sure that is is always white
+			g.setColor(Color.white);
+			index++;
+		}
+	}
+
+	private String formatHeaderString() {
+		StringBuilder str = new StringBuilder();
+
+		str.append(player.getName() + " (");
+		str.append(player.getCredits());
+		str.append("$)");
+
+		return str.toString();
+	}
+
+	private String formatAttrString(Attribute a) {
+		StringBuilder str = new StringBuilder();
+
+		str.append(a.name() + "(");
+		str.append(player.getAttribute(a) + ") \t");
+		str.append(a.getCost() + "$");
+		return str.toString();
+	}
+
+}
--- src/com/github/joakimpersson/tda367/gui/UpgradePlayerView.java
+++ src/com/github/joakimpersson/tda367/gui/UpgradePlayerView.java
@@ -1,15 +1,15 @@
 package com.github.joakimpersson.tda367.gui;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 
+import com.github.joakimpersson.tda367.Main;
 import com.github.joakimpersson.tda367.model.BombermanModel;
 import com.github.joakimpersson.tda367.model.IBombermanModel;
-import com.github.joakimpersson.tda367.model.constants.Attribute;
 import com.github.joakimpersson.tda367.model.player.Player;
 
 /**
@@ -21,6 +21,9 @@ public class UpgradePlayerView implements IUpgradePlayerView {
 
 	private IBombermanModel model = null;
 	private List<Player> players = null;
+	private List<UpgradePlayerPanelView> playerViews = null;
+	private final int POS_Y = 50;
+	private final int POS_X = 125;
 
 	public UpgradePlayerView() {
 		init();
@@ -29,46 +32,28 @@ public class UpgradePlayerView implements IUpgradePlayerView {
 	private void init() {
 		model = BombermanModel.getInstance();
 		players = model.getPlayers();
+		playerViews = new ArrayList<UpgradePlayerPanelView>();
+
+		int xDelta = Main.gameWidth / players.size();
+		int x = POS_X;
+
+		for (Player p : players) {
+			UpgradePlayerPanelView view = new UpgradePlayerPanelView(p, x,
+					POS_Y);
+
+			playerViews.add(view);
+			x += xDelta;
+		}
+
 	}
 
 	@Override
 	public void render(GameContainer container, Graphics g,
 			Map<Player, Integer> playerAttrIndex) {
-		int posY = 50;
-		int posX = 125;
-		int yDelta = 50;
-		int playerXDelta = container.getWidth() / players.size();
-		g.setColor(Color.white);
-		for (Player p : players) {
-			int attrIndex = playerAttrIndex.get(p);
-			drawPlayerStats(p, attrIndex, posX, posY, yDelta, g);
-			posX += playerXDelta;
+
+		for (UpgradePlayerPanelView view : playerViews) {
+			view.render(container, g, playerAttrIndex);
 		}
 	}
 
-	// TODO jocke yeah i know that it is a mess
-	private void drawPlayerStats(Player p, int attrIndex, int posX, int posY,
-			int yDelta, Graphics g) {
-
-		g.drawString(p.getName() + " (" + p.getCredits() + "$)", posX, posY);
-
-		posY += 25;
-
-		g.drawString("name(lvl), cost", posX, posY);
-
-		int index = 0;
-		for (Attribute a : p.getPermanentAttributes()) {
-			posY += yDelta;
-
-			if (index == attrIndex) {
-				g.setColor(Color.cyan);
-			}
-			g.drawString(
-					a.name() + "(" + p.getAttribute(a) + ") \t" + a.getCost()
-							+ "$", posX, posY);
-			// make sure that is is always white
-			g.setColor(Color.white);
-			index++;
-		}
-	}
 }
