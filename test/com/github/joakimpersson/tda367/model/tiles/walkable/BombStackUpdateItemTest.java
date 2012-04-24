17aa34e6012def23665136a05d19e7b3212bc37e
Joakim Persson <joakimperssson@gmail.com>
2012-04-24 10:25:35 +0200

    Added tests for getToughness, onFire, playerEnter, isWalkable  methods

--- test/com/github/joakimpersson/tda367/model/tiles/walkable/BombStackUpdateItemTest.java
+++ test/com/github/joakimpersson/tda367/model/tiles/walkable/BombStackUpdateItemTest.java
@@ -1,13 +1,21 @@
 package com.github.joakimpersson.tda367.model.tiles.walkable;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.github.joakimpersson.tda367.model.constants.Attribute;
+import com.github.joakimpersson.tda367.model.constants.Parameters;
+import com.github.joakimpersson.tda367.model.player.Player;
+import com.github.joakimpersson.tda367.model.tiles.Tile;
 import com.github.joakimpersson.tda367.model.tiles.walkable.BombStackUpdateItem;
+import com.github.joakimpersson.tda367.model.utils.Position;
 
 /**
  * 
@@ -21,6 +29,34 @@ public class BombStackUpdateItemTest {
 	@Before
 	public void setUp() throws Exception {
 		item = new BombStackUpdateItem();
+	}
+
+	@Test
+	public void testGetToughness() {
+		int tougness = item.getToughness();
+		assertEquals(0, tougness);
+	}
+
+	@Test
+	public void testOnFire() {
+		Tile tile = item.onFire();
+		assertThat(tile, is(instanceOf(Floor.class)));
+	}
+
+	@Test
+	public void testIsWalkable() {
+		assertTrue(item.isWalkable());
+	}
+
+	@Test
+	public void testPlayerEnter() {
+		Player player = new Player("Kalle", new Position(0, 0));
+		Tile tile = item.playerEnter(player);
+		assertThat(tile, is(instanceOf(Floor.class)));
+
+		int actual = player.getAttribute(Attribute.BombStack);
+		int expected = Parameters.INSTANCE.getStartingBombs() + 1;
+		assertEquals(expected, actual);
 	}
 
 	@Test
--- test/com/github/joakimpersson/tda367/model/tiles/walkable/RangeUpdateItemTest.java
+++ test/com/github/joakimpersson/tda367/model/tiles/walkable/RangeUpdateItemTest.java
@@ -1,13 +1,20 @@
 package com.github.joakimpersson.tda367.model.tiles.walkable;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.github.joakimpersson.tda367.model.constants.Attribute;
-import com.github.joakimpersson.tda367.model.tiles.walkable.RangeUpdateItem;
+import com.github.joakimpersson.tda367.model.constants.Parameters;
+import com.github.joakimpersson.tda367.model.player.Player;
+import com.github.joakimpersson.tda367.model.tiles.Tile;
+import com.github.joakimpersson.tda367.model.utils.Position;
 
 /**
  * 
@@ -22,6 +29,34 @@ public class RangeUpdateItemTest {
 	public void setUp() throws Exception {
 
 		item = new RangeUpdateItem();
+	}
+
+	@Test
+	public void testGetToughness() {
+		int tougness = item.getToughness();
+		assertEquals(0, tougness);
+	}
+
+	@Test
+	public void testOnFire() {
+		Tile tile = item.onFire();
+		assertThat(tile, is(instanceOf(Floor.class)));
+	}
+
+	@Test
+	public void testIsWalkable() {
+		assertTrue(item.isWalkable());
+	}
+
+	@Test
+	public void testPlayerEnter() {
+		Player player = new Player("Kalle", new Position(0, 0));
+		Tile tile = item.playerEnter(player);
+		assertThat(tile, is(instanceOf(Floor.class)));
+
+		int actual = player.getAttribute(Attribute.BombRange);
+		int expected = Parameters.INSTANCE.getInitBombRange() + 1;
+		assertEquals(expected, actual);
 	}
 
 	@Test
