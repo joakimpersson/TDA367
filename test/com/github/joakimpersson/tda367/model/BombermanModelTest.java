405f675493e9b6963baffc4ee931328077c42573
Viktor Anderling <natanande@gmail.com>
2012-04-26 11:31:08 +0200

    wrote a single test, not tested yet though

--- test/com/github/joakimpersson/tda367/model/BombermanModelTest.java
+++ test/com/github/joakimpersson/tda367/model/BombermanModelTest.java
@@ -1,10 +1,14 @@
 package com.github.joakimpersson.tda367.model;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.github.joakimpersson.tda367.model.player.Player;
 
 public class BombermanModelTest {
 
@@ -15,6 +19,27 @@ public class BombermanModelTest {
 		model = BombermanModel.getInstance();
 	}
 
+	@Test
+	public void testIsRoundOver() {
+		boolean test1;
+		boolean test2;
+		boolean test3;
+		
+		test1 = model.isRoundOver();
+		
+		List<Player> playerList = model.getPlayers();
+		for(Player p : playerList) {
+			p.playerHit();
+			p.playerHit();
+		}
+		test2 = model.isRoundOver();
+		
+		playerList.get(0).playerHit();
+		test3 = model.isRoundOver();
+
+		assertTrue(!test1 && !test2 && test3);
+	}
+	
 	@Test
 	public void testStartGame() {
 		fail("Not yet implemented");
@@ -39,7 +64,7 @@ public class BombermanModelTest {
 	public void testGetPlayers() {
 		fail("Not yet implemented");
 	}
-
+	
 	@After
 	public void tearDown() throws Exception {
 		model = null;
