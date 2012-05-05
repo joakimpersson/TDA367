21c39fa64c4641e6cbe6ddb381c0004e27d39c63
Adrian Bjugård <adrian@bjugard.se>
2012-05-06 01:29:00 +0200

    further complicated the Direction class!

--- src/com/github/joakimpersson/tda367/model/constants/Direction.java
+++ src/com/github/joakimpersson/tda367/model/constants/Direction.java
@@ -1,23 +1,25 @@
 package com.github.joakimpersson.tda367.model.constants;
 
 public enum Direction {
-	NORTH 		(0, -1, "north"),
-	NORTHEAST 	(0.75F, -0.75F, "north east"),
-	EAST 		(1, 0, "east"), 
-	SOUTHEAST 	(0.75F, 0.75F, "south east"), 
-	SOUTH 		(0, 1, "south"), 
-	SOUTHWEST 	(-0.75F, 0.75F, "south west"), 
-	WEST 		(-1, 0, "west"), 
-	NORTHWEST 	(-0.75F, -0.75F, "north west"), 
-	NONE 		(0, 0, "none");
+	NORTH 		(0, -1, "north", false),
+	NORTHEAST 	(0.75F, -0.75F, "north east", true),
+	EAST 		(1, 0, "east", false), 
+	SOUTHEAST 	(0.75F, 0.75F, "south east", true), 
+	SOUTH 		(0, 1, "south", false), 
+	SOUTHWEST 	(-0.75F, 0.75F, "south west", true), 
+	WEST 		(-1, 0, "west", false), 
+	NORTHWEST 	(-0.75F, -0.75F, "north west", true), 
+	NONE 		(0, 0, "none", false);
 	
 	private final float x, y;
 	private final String s;
+	private final boolean isDiagonal;
 	
-	Direction(float x, float y, String s) {
+	Direction(float x, float y, String s, boolean isDiagonal) {
 		this.x = x;
 		this.y = y;
 		this.s = s;
+		this.isDiagonal = isDiagonal;
 	}
 	
 	public float getX() {
@@ -30,5 +32,9 @@ public enum Direction {
 	
 	public String toString() {
 		return s;
+	}
+	
+	public boolean isDiagonal() {
+		return isDiagonal;
 	}
 }
