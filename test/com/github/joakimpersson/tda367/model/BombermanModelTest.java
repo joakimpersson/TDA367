ab5c715304d62866c93e6cace975dd280a800255
Viktor Anderling <natanande@gmail.com>
2012-05-08 23:13:13 +0200

    Re-enabled BombermanmodelTest for testing

--- test/com/github/joakimpersson/tda367/model/BombermanModelTest.java
+++ test/com/github/joakimpersson/tda367/model/BombermanModelTest.java
@@ -3,6 +3,7 @@ package com.github.joakimpersson.tda367.model;
 import static org.junit.Assert.*;
 
 import java.awt.Dimension;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
@@ -11,6 +12,9 @@ import org.junit.Test;
 
 import com.github.joakimpersson.tda367.model.constants.Parameters;
 import com.github.joakimpersson.tda367.model.player.Player;
+import com.github.joakimpersson.tda367.model.tiles.Tile;
+import com.github.joakimpersson.tda367.model.tiles.nonwalkable.*;
+import com.github.joakimpersson.tda367.model.tiles.walkable.*;
 import com.github.joakimpersson.tda367.model.utils.Position;
 
 public class BombermanModelTest {
@@ -20,15 +24,17 @@ public class BombermanModelTest {
 	@Before
 	public void setUp() throws Exception {
 		model = BombermanModel.getInstance();
-		List<Player> players = model.getPlayers();
+		List<Player> players = new ArrayList<Player>();
 		Dimension mapD = Parameters.INSTANCE.getMapSize();
 		players.add(new Player(1,"testPlayer1", new Position(1, 1)));
 		players.add(new Player(2,"testPlayer2", new Position(
 				(int)(mapD.getWidth()) -2, (int)(mapD.getHeight()) -2)));
+		model.startGame(players);
 	}
 
 	@Test
 	public void testIsRoundOver() {
+		
 		boolean test1;
 		boolean test2;
 		boolean test3;
@@ -55,6 +61,25 @@ public class BombermanModelTest {
 	
 	@Test
 	public void testMove() {
+//		String s = "";
+//		String t;
+//		Tile[][] map = model.getMap();
+//		for(int i = 0; i < map.length; i++) {
+//			for(int j = 0; j < map[0].length; j++) {
+//				if(map[i][j] instanceof Floor) {
+//					t = "F ";
+//				} else if(map[i][j] instanceof Pillar) {
+//					t = "P ";
+//				} else if(map[i][j] instanceof Box) {
+//					t = "B ";
+//				} else {
+//					t = ". ";
+//				}
+//				s = s + t;
+//			}
+//			s = s + "\n";
+//		}
+//		System.out.println(s);
 		fail("Not yet implemented");
 	}
 	
