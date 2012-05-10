5311c7e36c9d4be7025f27bd8cf8da962c651f65
Viktor Anderling <natanande@gmail.com>
2012-05-10 17:30:17 +0200

    Added music to the splashscreen

--- src/com/github/joakimpersson/tda367/controller/SplashState.java
+++ src/com/github/joakimpersson/tda367/controller/SplashState.java
@@ -1,5 +1,7 @@
 package com.github.joakimpersson.tda367.controller;
 
+import java.beans.PropertyChangeSupport;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
@@ -7,15 +9,18 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import com.github.joakimpersson.tda367.audio.AudioEventListener;
 import com.github.joakimpersson.tda367.controller.input.InputManager;
 import com.github.joakimpersson.tda367.gui.IView;
 import com.github.joakimpersson.tda367.gui.SplashView;
+import com.github.joakimpersson.tda367.model.constants.EventType;
 
 public class SplashState extends BasicGameState {
 
 	private IView view = null;
 	private int stateID = -1;
 	private InputManager inputManager = null;
+	private PropertyChangeSupport pcs;
 
 	public SplashState(int stateID) {
 		this.stateID = stateID;
@@ -26,12 +31,20 @@ public class SplashState extends BasicGameState {
 			throws SlickException {
 		view = new SplashView();
 		inputManager  = InputManager.getInstance();
+		this.pcs = new PropertyChangeSupport(this);
+		this.pcs.addPropertyChangeListener(AudioEventListener.getInstance());
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
 		view.render(container, g);
+	}
+	
+	@Override
+	public void enter(GameContainer container, StateBasedGame game)
+			throws SlickException {
+		pcs.firePropertyChange("play", null, EventType.TITLE_SCREEN);
 	}
 
 	@Override
