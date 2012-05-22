5ace2434720dce8ed7c26d6f8bf535cbaeb39494
Andreas Rolen <adderollen@hotmail.com>
2012-05-22 17:59:58 +0200

    Minor fixes in ImageLoader

--- src/com/github/joakimpersson/tda367/gui/ImageLoader.java
+++ src/com/github/joakimpersson/tda367/gui/ImageLoader.java
@@ -7,41 +7,32 @@ import java.util.Map;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
+/**
+ * A class handling all the imageloading in to the game.
+ * 
+ * @author adderollen
+ * 
+ */
 public class ImageLoader {
 
 	private static final String PATH = "res/images/";
 	private static ImageLoader instance = null;
 
-	private Map<String, Image> imageMap = new HashMap<String, Image>();
+	private Map<String, Image> imageMap;
 
+	/**
+	 * Creating an ImageLoader to load all the images in to the game.
+	 */
 	private ImageLoader() {
+		imageMap = new HashMap<String, Image>();
 		loadDirectory(PATH);
 	}
 
-	private void loadDirectory(String path) {
-		String directory = path.substring(PATH.length(), path.length());
-		File dir = new File(path);
-		for (File child : dir.listFiles()) {
-			if (".".equals(child.getName()) || "..".equals(child.getName())) {
-				continue;
-			} else if (child.isDirectory()) {
-				loadDirectory(path+child.getName()+"/");
-				continue;
-			}
-			loadImage(directory+child.getName());
-		}
-	}
-
-	private void loadImage(String s) {
-		try {
-			Image image = new Image("res/images/" + s);
-			image.setFilter(Image.FILTER_NEAREST);
-			imageMap.put(s.substring(0, s.length()-4), image);
-		} catch (SlickException e) {
-			System.out.println("File not found: " + s);
-		}
-	}
-
+	/**
+	 * Get the instance of ImageLoader
+	 * 
+	 * @return The ImageLoader instance.
+	 */
 	public static ImageLoader getInstance() {
 		if (instance == null) {
 			instance = new ImageLoader();
@@ -50,8 +41,51 @@ public class ImageLoader {
 		return instance;
 	}
 
-	public Image getImage(String image) {
-		return imageMap.get(image);
+	/**
+	 * Load a give directory in to the game.
+	 * 
+	 * @param path
+	 *            The directory that will be loaded.
+	 */
+	private void loadDirectory(String path) {
+		String directoryString = path.substring(PATH.length(), path.length());
+		File directory = new File(path);
+		for (File child : directory.listFiles()) {
+			if (".".equals(child.getName()) || "..".equals(child.getName())) {
+				continue;
+			} else if (child.isDirectory()) {
+				loadDirectory(path + child.getName() + "/");
+				continue;
+			}
+			loadImage(directoryString + child.getName());
+		}
+	}
+
+	/**
+	 * Load an image with a given name.
+	 * 
+	 * @param imageName
+	 *            The name of the image that will be load.
+	 */
+	private void loadImage(String imageName) {
+		try {
+			Image image = new Image(PATH + imageName);
+			image.setFilter(Image.FILTER_NEAREST);
+			imageMap.put(imageName.substring(0, imageName.length() - 4), image);
+		} catch (SlickException e) {
+			System.out.println("File not found: " + imageName);
+		}
+	}
+
+	/**
+	 * Get an image with a given name.
+	 * 
+	 * @param imageName
+	 *            The name of the image.
+	 * @return The image with the given name.
+	 */
+	public Image getImage(String imageName) {
+		return imageMap.get(imageName);
 	}
 
 }
