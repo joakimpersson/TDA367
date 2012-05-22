90e52c211a9e37296edc8485114d05c0729f7eee
Andreas Rolen <adderollen@hotmail.com>
2012-05-22 21:28:35 +0200

    Minor fixes in X360OsDep and javadoc

--- src/com/github/joakimpersson/tda367/controller/input/osdependant/X360OsDep.java
+++ src/com/github/joakimpersson/tda367/controller/input/osdependant/X360OsDep.java
@@ -2,50 +2,92 @@ package com.github.joakimpersson.tda367.controller.input.osdependant;
 
 import org.newdawn.slick.Input;
 
+/**
+ * Keymapping for Xbox360 controller
+ * 
+ * @author adderollen
+ * 
+ */
 public class X360OsDep {
 	private final static String platform = System.getProperty("os.name");
 	private final static String MAC = "Mac OS X";
+	private final static String WINDOWS = "Windows";
 
-	// should be mapped to START
+	/**
+	 * Get the proceed keycode.
+	 * 
+	 * @return The proceed keycode
+	 */
 	public static int getProceed() {
+		// should be mapped to START
 		if (platform.equals(MAC)) {
 			return 4;
-		} else return 7;
+		} else if (platform.startsWith(WINDOWS)) {
+			return 7;
+		}
+		return 0;
 	}
-	
-	// should be mapped to A
+
+	/**
+	 * Get the primary action keycode
+	 * 
+	 * @return The primary action keycode
+	 */
 	public static int getPrimaryAction() {
+		// should be mapped to A
 		if (platform.equals(MAC)) {
 			return 11;
-		} else return 0;
+		} else if (platform.startsWith(WINDOWS)) {
+			return 0;
+		}
+		return 0;
 	}
-	
-	// should be mapped to X
+
+	/**
+	 * Get the secondary action keycode
+	 * 
+	 * @return The secondary action keycode
+	 */
 	public static int getSecondaryAction() {
+		// should be mapped to X
 		if (platform.equals(MAC)) {
 			return 13;
-		} else return 2;
+		} else if (platform.startsWith(WINDOWS)) {
+			return 2;
+		}
+		return 0;
 	}
-	
+
+	/**
+	 * Get the direction of any input keycode
+	 * 
+	 * @param input
+	 *            The input that will be checked
+	 * @param controllerId
+	 *            The controller identity
+	 * @return A String containing the direction the given input means
+	 */
 	public static String getDPadButtonPressed(Input input, int controllerId) {
 		if (platform.equals(MAC)) {
-			if (input.isControlPressed(4, controllerId))
+			if (input.isControlPressed(4, controllerId)) {
 				return "up";
-			else if (input.isControlPressed(5, controllerId))
+			} else if (input.isControlPressed(5, controllerId)) {
 				return "down";
-			else if (input.isControlPressed(6, controllerId))
+			} else if (input.isControlPressed(6, controllerId)) {
 				return "left";
-			else if (input.isControlPressed(7, controllerId))
+			} else if (input.isControlPressed(7, controllerId)) {
 				return "right";
-		} else {
-			if (input.isControlPressed(2, controllerId))
+			}
+		} else if (platform.startsWith(WINDOWS)) {
+			if (input.isControlPressed(2, controllerId)) {
 				return "up";
-			else if (input.isControlPressed(3, controllerId))
+			} else if (input.isControlPressed(3, controllerId)) {
 				return "down";
-			else if (input.isControlPressed(0, controllerId))
+			} else if (input.isControlPressed(0, controllerId)) {
 				return "left";
-			else if (input.isControlPressed(1, controllerId))
+			} else if (input.isControlPressed(1, controllerId)) {
 				return "right";
+			}
 		}
 		return "none";
 	}
