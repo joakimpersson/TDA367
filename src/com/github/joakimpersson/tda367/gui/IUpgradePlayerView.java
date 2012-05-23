c5f1233615a0ae0d8657739a0c307f15af69c4bd
Adrian Bjugård <adrian@bjugard.se>
2012-05-23 11:51:50 +0200

    JavaDoc for IUpgradePlayerView

--- src/com/github/joakimpersson/tda367/gui/IUpgradePlayerView.java
+++ src/com/github/joakimpersson/tda367/gui/IUpgradePlayerView.java
@@ -11,11 +11,25 @@ import com.github.joakimpersson.tda367.model.constants.Attribute;
  * Interface for the UpgradePlayerView class
  * 
  * @author rekoil
- *
+ * 
  */
 public interface IUpgradePlayerView {
 	/**
-	 * Render method for the upgrade player view
+	 * Render method for the upgrade player view, the first integer in each of
+	 * the maps is representative of the player with this index
+	 * 
+	 * @param container
+	 *            The container for the game
+	 * @param g
+	 *            The g context to draw with
+	 * @param playerAttrIndex
+	 *            The indexes of the players
+	 * @param playerReadyness
+	 *            The status of the different players
+	 * @param playerCredits
+	 *            The credits for the players
+	 * @param upgradeMap
+	 *            The current status for an attribute upgrade
 	 */
 	void render(GameContainer container, Graphics g,
 			Map<Integer, Integer> playerAttrIndex,
